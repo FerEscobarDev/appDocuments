e59d9f4f1c91fcbafa44bbf3a19194a89e51bd80
FerEscobarDev <jeiesc04@outlook.com>
2023-03-28 20:17:09 -0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormDocumentComponent } from './documents/form-document/form-document.component';
+import { ListDocumentsComponent } from './documents/list-documents/list-documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: ListDocumentsComponent },
+    { path: 'documents', component: ListDocumentsComponent },
+    { path: 'document/create', component: FormDocumentComponent },
+    { path: 'document/edit', component: FormDocumentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ListDocumentsComponent } from './documents/list-documents/list-documents.component';
+import { FormDocumentComponent } from './documents/form-document/form-document.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ListDocumentsComponent,
+    FormDocumentComponent,
   ],
   imports: [
     BrowserModule,
